e10be717c1a525eb92d835be596a33e0a95e2433
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-17 20:53:01 +0300

    Update sponsor.js

--- commands/🔰 Info/sponsor.js
+++ commands/🔰 Info/sponsor.js
@@ -17,16 +17,16 @@ module.exports = {
 				.setTimestamp()
 				.setFooter("Bittmax.de | Code  'x10'  == -5%", "https://cdn.discordapp.com/icons/784157254847954964/482b9e96414509756fc0192829382776.webp")
 				.setImage("https://cdn.discordapp.com/attachments/781568883051462666/800441576442691644/bittmax.png")
-				.setTitle("Milrato | Service -- Bittmax")
+				.setTitle("Shvan | Service -- ProgressHome")
 				.setURL("https://milrato.eu")
 				.setDescription(`
-				> [Milrato Service](https://discord.gg/terWzJjF7p) made this Bot for us. It is free and also hosted on their sponsor: [Bittmax.de](https://bittmax.de)
+				> [Shvan Service](https://discord.gg/cWvV7xzRnP) made this Bot for us. It is free and also hosted on their sponsor: [ProgressHome](https://discord.gg/progresshome)
 
 				> If you use the code: **\`x10\`** their, then you'll get at least 5% off everything!
 
-				> Check out their [Service-Website](https://milrato) and their [Discord](https://discord.gg/terWzJjF7p) to get your own Bot too!
+				> Check out their [Service-Website]() and their [Discord](https://discord.gg/cWvV7xzRnP) to get your own Bot too!
 
-				> It's hosted **24/7**, as already said, thanks to their Sponsor [**Bittmax.de**](https://bittmax.de)!`)
+				> It's hosted **24/7**, as already said, thanks to their Sponsor [**ProgressHome**](https://discord.gg/progresshome)!`)
 			).catch(e => console.log(String(e.stack).yellow));
 			message.channel.send(new Discord.MessageEmbed()
 				.setColor(ee.color)
@@ -46,9 +46,9 @@ module.exports = {
 		    > **>>** TEAMSPEAK SERVERS
 		    > **>>** Linux & Windows Root Servers
 
-		    [**Discord Server:**](https://discord.com/invite/4dGuGXK4A4)
-		    [**Website:**](https://mc-host24.de/user/affiliate/3121)
-		    [**__SPONSOR LINK!__**](https://mc-host24.de/donate/tomato)
+		    [**Discord Server:**](https://discord.gg/cWvV7xzRnP)
+		    [**Website:**](Coomeing Soon)
+		    [**__SPONSOR LINK!__**](https://discord.gg/progresshome))
 		    `)
 				.setImage("https://cdn.discordapp.com/attachments/758007636720353380/758055267735699618/MC-HOST24-Logo-24-fett.png")
 				.setFooter("Mc-Host24", "https://cdn.discordapp.com/icons/619465432293965864/4c101b43466708cec4506938154a4e34.webp")
