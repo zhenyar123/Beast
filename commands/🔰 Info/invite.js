859567f43eebc5e95e95e40c879f67ad8b180e9a
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-17 20:42:12 +0300

    Update invite.js

--- commands/🔰 Info/invite.js
+++ commands/🔰 Info/invite.js
@@ -18,7 +18,7 @@ module.exports = {
         .setTitle(":heart: Thanks for inviting me!")
         .setFooter(ee.footertext, ee.footericon)
         .setURL("https://discord.com/api/oauth2/authorize?client_id=742672021422342165&permissions=8&scope=bot")
-        .setDescription("[Click here](https://discord.com/api/oauth2/authorize?client_id=742672021422342165&permissions=8&scope=bot)")
+        .setDescription("[Click here](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)")
       );
     } catch (e) {
       console.log(String(e.stack).bgRed)
